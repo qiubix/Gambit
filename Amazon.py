61f1963216d05c99065d78e4f70ef80f703c85ce
qiubix <qiubix@gmail.com>
2015-11-22 20:13:18 +0100

    Count connected components

--- Amazon.py
+++ Amazon.py
@@ -12,14 +12,27 @@ class AmazonGraphAnalyzer:
         self.amazonGraph = amazonGraph
         return amazonGraph
 
-    def numberOfNodes(self):
+    def getNumberOfNodes(self):
         return self.amazonGraph.number_of_nodes()
 
-    def numberOfEdges(self):
+    def getNumberOfEdges(self):
         return self.amazonGraph.number_of_edges()
+
+    def getNumberOfConnectedComponents(self):
+        return nx.number_connected_components(self.amazonGraph)
+
+    def getLargestConnectedComponent(self):
+        return max(nx.connected_components(self.amazonGraph), key=len)
+
+    def exportToPajek(self):
+        nx.write_pajek(self.amazonGraph, 'amazon-graph.net')
 
 if __name__ == '__main__':
     analyzer = AmazonGraphAnalyzer()
     amazonGraph = analyzer.importGraph("com-amazon.ungraph.txt")
-    print amazonGraph.number_of_nodes(), "nodes"
-    print amazonGraph.number_of_edges(), "edges"
+    print "Amazon product co-purchasing network"
+    print nx.info(amazonGraph)
+    print "Amazon graph has:", analyzer.getNumberOfNodes(), "nodes"
+    print "Amazon graph has:", analyzer.getNumberOfEdges(), "edges"
+    print "Number of connected components: ", analyzer.getNumberOfConnectedComponents()
+    print "Largest CC size: ", len(analyzer.getLargestConnectedComponent())
--- tests/AmazonTest.py
+++ tests/AmazonTest.py
@@ -3,8 +3,8 @@ import unittest
 from Amazon import AmazonGraphAnalyzer
 
 
-# TODO: import graph - check number of nodes and edges
-# TODO: count connected components
+# + import graph - check number of nodes and edges
+# + count connected components
 # TODO: get biggest connected component (cound no of nodes and edges)
 # TODO: get mean assortativity
 # TODO: get Pearson assortativity
@@ -16,8 +16,15 @@ class TestImportGraph(unittest.TestCase):
         self.analyzer.importGraph("test-ungraph.txt")
 
     def test_import_graph(self):
-        self.assertEquals(self.analyzer.numberOfNodes(), 4)
-        self.assertEquals(self.analyzer.numberOfEdges(), 5)
+        self.assertEquals(self.analyzer.getNumberOfNodes(), 6)
+        self.assertEquals(self.analyzer.getNumberOfEdges(), 7)
+
+    def test_number_of_connected_components(self):
+        self.assertEquals(self.analyzer.getNumberOfConnectedComponents(), 2)
+
+    def test_get_largest_connected_component(self):
+        largest_cc = self.analyzer.getLargestConnectedComponent()
+        self.assertEquals(len(largest_cc), 4)
 
 
 if __name__ == '__main__':
